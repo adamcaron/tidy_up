6a32995cbd389296f021f334a63e29a9785b5755
Adam Caron <adamwcaron@gmail.com>
2015-09-12 15:38:13 -0600

    Setup simplecov, capybara, db-cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,40 @@
+require 'simplecov'
+SimpleCov.start 'rails'
+
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 
 RSpec.configure do |config|
-
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
+  end
+
+  require 'capybara/rspec'
+  config.include Capybara::DSL
+
+  require 'database_cleaner'
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, :js => true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
   end
 
 end
